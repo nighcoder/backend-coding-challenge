d52ca214141ae7929f598cc5e4715b2ea3c9eac5
Daniel Ciumberică <ciumbi.ionut@gmail.com>
2020-02-10 03:20:17 -0500

    Added geographic distance function

--- src/backend_coding_challenge/core.clj
+++ src/backend_coding_challenge/core.clj
@@ -5,7 +5,23 @@
 
 (def data
   (with-open [file (io/reader (io/resource "data/cities_canada-usa.tsv"))]
-    (doall (csv/read-csv file))))
+    (doall (csv/read-csv file :separator \tab :quote \`))))
+
+(defn deg->rad
+  [a]
+  (/ (* a Math/PI) 180))
+
+(defn geo-distance
+  [lat1 long1 lat2 long2]
+  (let [R-TERRA 6371] ;; Radius of Earth [km]
+    (* R-TERRA
+       (Math/acos
+         (+ (* (Math/sin (deg->rad lat1))
+               (Math/sin (deg->rad lat2)))
+            (* (Math/cos (deg->rad lat1))
+               (Math/cos (deg->rad lat2))
+               (Math/cos (- (deg->rad long2)
+                            (deg->rad long1)))))))))
 
 (defn -main
   "I don't do a whole lot ... yet."
