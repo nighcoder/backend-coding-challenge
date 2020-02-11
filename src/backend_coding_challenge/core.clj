066c0dd37050dfc2e0d58396e6ce55f150a529de
Daniel Ciumberică <ciumbi.ionut@gmail.com>
2020-02-11 05:54:27 -0500

    Replaced the name-distance function by name-min-distance that takes a query and a list of names and returns a pair of minimum insert distance from query to name, along with that name. Changed name-score function to work with the result of above mentioned function. Added country and admin function to return the country name and administrative region respectively. Added matches? predicate function to filter the results. Added result function to prepare the output

--- src/backend_coding_challenge/core.clj
+++ src/backend_coding_challenge/core.clj
@@ -8,6 +8,21 @@
   (with-open [file (io/reader (io/resource "data/cities_canada-usa.tsv"))]
     (doall (csv/read-csv file :separator \tab :quote \`))))
 
+(def CA-ADM
+  {"01" "AB"
+   "02" "BC"
+   "03" "MB"
+   "04" "NB"
+   "05" "NL"
+   "07" "NS"
+   "08" "ON"
+   "09" "PE"
+   "10" "QC"
+   "11" "SK"
+   "12" "YT"
+   "13" "NT"
+   "14" "NU"})
+
 (defn deg->rad
   [a]
   (/ (* a Math/PI) 180))
@@ -25,18 +40,38 @@
                (Math/cos (- (deg->rad long2)
                             (deg->rad long1)))))))))
 
-(defn name-distance
-  [name1 name2]
-  "Returns the insert distance between two names"
-  (if (s/includes? name2 name1)
-    (count (s/replace-first name2 name1 ""))
-    (when (s/includes? name1 name2)
-      (count (s/replace-first name1 name2 "")))))
+(defn name-min-distance
+  [query names]
+  "Returns a pair of name which has the minimum distance from query, along with that distance value"
+  (loop [names names
+         res-name nil
+         res-dist nil]
+    (if (seq names)
+      (let [name (first names)
+            new-dist (count (s/replace-first name query ""))]
+        (if (and (s/includes? name query)
+                 (or (not res-dist)
+                     (> res-dist new-dist)))
+          ;; If query is substring of name AND EITHER we have no distance result OR the new distance result is
+          ;; smaller than the previous value recur with new values
+          (recur (rest names) name new-dist)
+          ;; If query is not a substring of name OR it is one, but the new distance is bigger than the previous one
+          ;; recur with the old values
+          (recur (rest names) res-name res-dist)))
+      (list res-name res-dist))))
+
+;; Obsolete after defining name-min-distance
+;(defn name-distance
+;  [name1 name2]
+;  "Returns the insert distance between two names"
+;  (if (s/includes? name2 name1)
+;    (count (s/replace-first name2 name1 ""))
+;    (when (s/includes? name1 name2)
+;      (count (s/replace-first name1 name2 "")))))
 
 (defn name-score
-  [query match]
-  (- 1 (/ (name-distance query match)
-          (count match))))
+  [[name distance]]
+  (- 1 (/ distance (count name))))
 
 (defn names
   [entry]
@@ -50,8 +85,40 @@
          (cons ascii)
          (cons name)
          set)))
+
+(defn country
+  [entry]
+  "Returns the country name in entry"
+  (let [code (->> entry (drop 8) first)]
+    (case code
+      "CA" "Canada"
+      "US" "USA")))
+
+(defn admin
+  [entry]
+  "Returns the ISO code for administrative region of country"
+  (let [code (->> entry (drop 10) first)]
+    (case (country entry)
+      "Canada" (CA-ADM code)
+      "USA" code)))
  
- 
+(defn matches?
+  [query entry]
+  "Returns true if query matches entry (row of data)"
+  (some #(s/includes? % query) (names entry)))
+
+(defn result
+  [query entry & coord]
+  {:name (str (second entry)
+              ", "
+              (admin entry)
+              ", "
+              (country entry))
+   :latitude (->> entry (drop 4) first)
+   :longitude (->> entry (drop 5) first)
+   :population (->> entry (drop 14) first)
+   :score (name-score query (names entry))})
+              
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
