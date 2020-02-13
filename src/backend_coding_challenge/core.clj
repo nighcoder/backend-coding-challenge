9221324f9a3bd644722767c30fce0372d89713bd
Daniel Ciumberică <ciumbi.ionut@gmail.com>
2020-02-13 03:28:43 -0500

    Changed DATA constant to parse each entry in database and updated the functions to use the underlying format. Removed obsolete functions name-min-distance, admin and country. Updated definitions for functions: name-distance, name-score, names and result.

--- src/backend_coding_challenge/core.clj
+++ src/backend_coding_challenge/core.clj
@@ -6,7 +6,29 @@
 
 (def DATA
   (with-open [file (io/reader (io/resource "data/cities_canada-usa.tsv"))]
-    (doall (csv/read-csv file :separator \tab :quote \`))))
+    (let [data (csv/read-csv file :separator \tab :quote \`)]
+      (doall
+        (for [[id name ascii alt-names lat lon feat-class feat-code country cc2
+               admin1 admin2 admin3 admin4 population elev dem tz mod-at] (rest data)]
+          [(Integer/parseInt id)
+           name
+           ascii
+           (s/split alt-names #",")
+           (Float/parseFloat lat)
+           (Float/parseFloat lon)
+           (first feat-class)
+           feat-code
+           country
+           cc2
+           admin1
+           admin2
+           admin3
+           admin4
+           (Integer/parseInt population)
+           (when (seq elev) (Integer/parseInt elev))
+           (when (seq dem) (Integer/parseInt dem))
+           tz
+           mod-at]))))) 
 
 (def CA-ADM
   {"01" "AB"
@@ -40,68 +62,28 @@
                (Math/cos (- (deg->rad long2)
                             (deg->rad long1)))))))))
 
-(defn name-min-distance
-  [query names]
-  "Returns a pair of name which has the minimum distance from query, along with that distance value"
-  (loop [names names
-         res-name nil
-         res-dist nil]
-    (if (seq names)
-      (let [name (first names)
-            new-dist (count (s/replace-first name query ""))]
-        (if (and (s/includes? name query)
-                 (or (not res-dist)
-                     (> res-dist new-dist)))
-          ;; If query is substring of name AND EITHER we have no distance result OR the new distance result is
-          ;; smaller than the previous value recur with new values
-          (recur (rest names) name new-dist)
-          ;; If query is not a substring of name OR it is one, but the new distance is bigger than the previous one
-          ;; recur with the old values
-          (recur (rest names) res-name res-dist)))
-      (list res-name res-dist))))
+(defn names
+  [[_ name ascii alt-names & _]]
+  "Returns the normalized name list for row of data"
+  (into #{} (map (comp s/trim s/lower-case) (conj alt-names name ascii))))
 
-;; Obsolete after defining name-min-distance
-;(defn name-distance
-;  [name1 name2]
-;  "Returns the insert distance between two names"
-;  (if (s/includes? name2 name1)
-;    (count (s/replace-first name2 name1 ""))
-;    (when (s/includes? name1 name2)
-;      (count (s/replace-first name1 name2 "")))))
+(defn name-distance
+  [query -name]
+  "Returns the insert distance between a name and a query"
+  (when (s/includes? -name query)
+    (count (s/replace-first -name query ""))))
 
 (defn name-score
-  [[name distance]]
-  (- 1 (/ distance (count name))))
+  [query entry]
+  (let [parser (fn [query -name]
+                 (if-let [dist (name-distance query -name)]
+                   (- 1 (/ dist (count -name)))
+                   0))
+        names (into #{} (map (comp s/trim s/lower-case) (subvec entry 1 3)))
+        res (mapv (partial parser query) names)
+        alt-names (into #{} (map (comp s/trim s/lower-case)) (entry 3))]
+    (float (reduce max (into res (map (comp (partial * 1/2) (partial parser query)) alt-names))))))
 
-(defn names
-  [entry]
-  "Returns the normalized name list for row of data"
-  (let [alt-names (->> entry (drop 3) first)
-        nalt-names (when (seq alt-names)
-                     (->> (s/split alt-names #",") (map s/trim) (map s/lower-case)))
-        name (-> entry second s/trim s/lower-case)
-        ascii (->> entry (drop 2) first s/trim s/lower-case)]
-    (->> nalt-names
-         (cons ascii)
-         (cons name)
-         set)))
-
-(defn country
-  [entry]
-  "Returns the country name in entry"
-  (let [code (->> entry (drop 8) first)]
-    (case code
-      "CA" "Canada"
-      "US" "USA")))
-
-(defn admin
-  [entry]
-  "Returns the ISO code for administrative region of country"
-  (let [code (->> entry (drop 10) first)]
-    (case (country entry)
-      "Canada" (CA-ADM code)
-      "USA" code)))
- 
 (defn matches?
   [query entry]
   "Returns true if query matches entry (row of data)"
@@ -109,15 +91,20 @@
 
 (defn result
   [query entry & coord]
-  {:name (str (second entry)
-              ", "
-              (admin entry)
-              ", "
-              (country entry))
-   :latitude (->> entry (drop 4) first)
-   :longitude (->> entry (drop 5) first)
-   :population (->> entry (drop 14) first)
-   :score (name-score query (names entry))})
+  (let [[_ -name  _ _ lat lon _ _ country _ admin _ _ _ population & _] entry]
+    {:name (str -name
+                ", "
+                (case country
+                  "CA" (CA-ADM admin)
+                  "US" admin)
+                ", "
+                (case country
+                  "CA" "Canada"
+                  "US" "USA"))
+     :latitude lat
+     :longitude lon
+     :population population
+     :score (name-score query entry)}))
               
 (defn -main
   "I don't do a whole lot ... yet."
